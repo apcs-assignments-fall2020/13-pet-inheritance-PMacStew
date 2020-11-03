7c367179b9ffadcd32136f7bf193c3ed90cda32b
PMacStew <23pmcclure@athenian.org>
2020-11-03 18:14:04 +0000

    forgot to submit

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,35 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
 
+    public Dog() {
+        super();
+        this.breed = "Beagle";
+    }
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
     // toString method
-
+    public String toString() {
+        return "Name: " + this.getName() + ". Age: " + this.getAge() + ". Breed: " + this.breed;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        if (breed.trim().length() != 0)
+            this.breed = breed;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class DogClient {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        Pet pet1 = new Pet("John", 15);
+        System.out.print("Enter dogs name: ");
+        String name = scan.next();
+        System.out.print("Enter dogs age: ");
+        int age = scan.nextInt();
+        System.out.print("Enter dogs breed: ");
+        String breed = scan.next();
+        Dog dog1 = new Dog(name, age, breed);
+        System.out.println(dog1.toString());
+        dog1.makeNoise();
+        pet1.makeNoise();
     }
 }
